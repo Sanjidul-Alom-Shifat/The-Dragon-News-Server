7d7e33d5ddce9de510b5699def85934604ed111c
Sanjidul-Alom-Shifat <sanjidul.alom.shifat.official@gmail.com>
2023-04-28 23:18:34 +0600

    adding news data for server

--- index.js
+++ index.js
@@ -4,17 +4,39 @@ const cors = require('cors');
 const port =  5000;
 
 const categories = require('./data/catagories.json');
+const news = require('./data/news.json');
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('categories')
-    console.log(categories);
+    res.send('categories of dragon news website')
+    // console.log(categories);
 });
 
 app.get('/categories', (req, res) =>{
     console.log(categories);
     res.send(categories);
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectid = news.find(n => n._id === id);
+    res.send(selectid);
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(news);
+    }
+    else {
+        const Categorynews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(Categorynews);
+    }
 })
 
 
